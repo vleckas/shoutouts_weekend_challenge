76ed7a5d0b75e24ad2b524f01f5a237f1e376754
Vas Leckas <vas.leckas@gmail.com>
2015-08-28 18:00:32 -0500

    make it better

---
+++ public/javascripts/app.js
@@ -0,0 +1,35 @@
+// When the DOM is ready
+$(document).ready(function () {
+
+    // get our content UL
+    var $results = $('#content');
+
+
+    // On doc ready, do an ajax call to get all customers
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: '/users'
+    }).done(function (data) {
+        // same as success
+
+        // for each customer, create an LI with their name in a P tag
+
+        data.forEach(function (elem) {
+
+            // create a new LI node with the id set to the customer id
+            var $li = $('<li>', {id: elem.id});
+            var $p = $('<p>');
+            $p.text(elem.firstName + ' ' + elem.lastName + ' ' + elem.shoutOut + ' ' + elem.id);
+            $li.append($p);
+            $results.append($li);
+
+        });
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        // same as error
+        console.log('There was an error: ', errorThrown);
+    }).always(function () {
+        // same as complete
+        console.log('Ajax Complete!');
+    })
+});
---
+++ routes/users.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+/* GET users listing. */
+router.get('/:id?', function(req, res, next) {
+
+  var id = req.params.id;
+
+  var file = path.join(__dirname, '../models/users.json');
+  fs.readFile(file, 'utf8', function(err, data){
+    if(err){
+      next(err);
+    } else {
+      var obj = JSON.parse(data);
+      var user = obj; // if no user is found, then return all customers
+
+      obj.forEach(function(elem){
+        if(elem.id == id){
+          user = elem;
+        }
+      });
+
+      res.json(user);
+    }
+  })
+});
+
+module.exports = router;
