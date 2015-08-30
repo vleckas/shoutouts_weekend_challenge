9adb381b95524487242ca2af9e6bd088fa27deb7
Vas Leckas <vas.leckas@gmail.com>
2015-08-30 18:25:19 -0500

    make it better

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -22,6 +22,17 @@ $(document).ready(function () {
             $p.text(elem.firstName + ': ' + elem.shoutOut);
             $p.append($p);
             $shoutouts.append($p);
+
+            $(function() {
+                $('p').on('click', function() {
+                    $(this).animate({
+                        opacity: 0.0,
+                        paddingLeft: '+=80'
+                    }, 500, function() {
+                        $(this).remove();
+                    });
+                });
+            });
         });
 
     }).fail(function (jqXHR, textStatus, errorThrown) {
